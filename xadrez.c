5d4d86db56dc385529d55b23463ea4184f2782ec
smileynos <israelsantanna2@outlook.com>
2025-04-14 00:40:56 +0000

    NOVO commit

--- xadrez.c
+++ xadrez.c
@@ -1,46 +1,57 @@
 #include <stdio.h>
-
-
+ 
+     
 int main() {
-
     int opcao;
-
-    // Menu de opções
+    int movimentocavalo;
     do{
         printf("Inicie o jogo xadrez\n");
         printf("Escolha a opção da peça que você quer movimentar: \n");
         printf("1. Movimentar o Bispo \n");
         printf("2. Movimentar a Rainha \n");
         printf("3. Movimentar a Torre\n");
+        printf("4. Movimentar o Cavalo\n");
         scanf("%d", &opcao);
         
         switch (opcao)
         {
         case 1:
-            printf("Você escolheu se movimentar com o bispo\n");
+            printf("Você escolheu se movimentar com o bispo:\n");
             for(opcao = 0; opcao <5; opcao++){
-                printf(" 5 casas para cima direita\n");
+                printf("Direita\n");
             }
             
         break;
 
         case 2:
-            printf("Você escolheu se movimentar com a Rainha\n");
+            printf("Você escolheu se movimentar com a Rainha:\n");
             for(opcao = 0; opcao <8; opcao++){
-                printf("8 casas Para Esquerda\n");
+                printf("Esquerda\n");
             }
         break;
         case 3:
-            printf("Você escolheu se movimentar com a Torre\n");
+            printf("Você escolheu se movimentar com a Torre:\n");
             for(opcao = 0; opcao <5; opcao++){
-                printf(" 5 casas para frente\n");
+                printf("Frente\n");
+            }
+        break;
+        case 4:
+            printf("Você escolheu se movimentar com o cavalo:\n");
+            for(opcao = 0; opcao <2; opcao++){
+                printf("Cima\n");
+                for (movimentocavalo = 0; movimentocavalo <opcao; movimentocavalo++){
+                    printf("Esqueda\n");
+                }
             }
         break;
         default:
         printf("Nenhuma opção escolhida\n \n"); 
             break;
         }
-    }while (opcao == 4);
+    }while (opcao > 4 );
 
+    
     return 0;
+      
+
 }
